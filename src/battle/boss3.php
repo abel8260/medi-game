7574b57d17c327e40fd4d309aaa9b533fbd0be0f
abel8260 <abelvbarbosa@gmail.com>
2021-07-05 08:59:31 -0300

    v(1.00.02)

--- src/battle/boss3.php
+++ src/battle/boss3.php
@@ -1,3 +1,12 @@
+<?php 
+  session_start();
+
+    $nivelar= $_SESSION['tempo'];
+    $nivel = 45;
+    $_SESSION['tempo']= $nivel + $nivelar;
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -7,63 +16,66 @@
 	</head>
 
 	<body style="background-image:url('../../img/background.png');background-size:100%; ">
-  <div class="w3-border">
-        <div id="myBar1" class="w3-container w3-red"  style="top:0px; left:0%; height:24px;width:100%"> 9 minutos</div>
-    </div> 
-    <div class="w3-border">
-        <div id="myBar2" class="w3-container w3-white" style="top:0px;left:20%;height:24px;width:100%">27 minutos</div>
-    </div> 
-    <div class="w3-border">
-        <div id="myBar3" class="w3-container w3-blue" style="top:0px;left:80%;height:24px;width:100%">9 minutos</div>
-    </div> 
+        
+          <div class="w3-border">
+              <div id="myBar1" class="w3-container w3-red"  style="top:0px; left:0%; height:24px;width:100%"> 9 minutos</div>
+          </div> 
+          <div class="w3-border">
+              <div id="myBar2" class="w3-container w3-white" style="top:0px;left:20%;height:24px;width:100%">27 minutos</div>
+          </div> 
+          <div class="w3-border">
+              <div id="myBar3" class="w3-container w3-blue" style="top:0px;left:80%;height:24px;width:100%">9 minutos</div>
+          </div> 
+
+
         <img class="solo w3-spin"  width='20%' height='20%' src='../../img/boss-3.png' style='left:40%; top:45%; position:absolute; '/>
 
 
-        <button class="w3-button w3-light-grey" onclick="move1()">fatality</button>
-        <button class="w3-button w3-light-grey" onclick="move2()">atacar o life</button>
-        <button class="w3-button w3-light-grey" onclick="move3()">atacar escudo</button>
+          <button class="w3-button w3-light-grey" onclick="move1()">fatality</button>
+          <button class="w3-button w3-light-grey" onclick="move2()">atacar o life</button>
+          <button class="w3-button w3-light-grey" onclick="move3()">atacar escudo</button>
 
         <script>
-function move1() {
-  var elem = document.getElementById("myBar1");
-  var width = 1;
-  var id = setInterval(frame, 54000);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width++;
-      elem.style.width = width + '%';
-    }
-  }
-}
-function move2() {
-  var elem = document.getElementById("myBar2");
-  var width = 1;
-  var id = setInterval(frame, 162000);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width++;
-      elem.style.width = width + '%';
-    }
-  }
-}
-function move3() {
-  var elem = document.getElementById("myBar3");
-  var width = 1;
-  var id = setInterval(frame, 54000);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width++;
-      elem.style.width = width + '%';
-    }
-  }
-}
-</script>
+          function move1() {
+            var elem = document.getElementById("myBar1");
+            var width = 1;
+            var id = setInterval(frame, 54000);
+            function frame() {
+              if (width >= 100) {
+                clearInterval(id);
+              } else {
+                width++;
+                elem.style.width = width + '%';
+              }
+            }
+          }
+          function move2() {
+            var elem = document.getElementById("myBar2");
+            var width = 1;
+            var id = setInterval(frame, 162000);
+            function frame() {
+              if (width >= 100) {
+                clearInterval(id);
+              } else {
+                width++;
+                elem.style.width = width + '%';
+              }
+            }
+          }
+          function move3() {
+            var elem = document.getElementById("myBar3");
+            var width = 1;
+            var id = setInterval(frame, 54000);
+            function frame() {
+              if (width >= 100) {
+                clearInterval(id);
+              } else {
+                width++;
+                elem.style.width = width + '%';
+              }
+            }
+          }
+        </script>
 
 
 
--- src/home/index.php
+++ src/home/index.php
@@ -1,119 +1,147 @@
+<?php 
+    session_start(); 
+        if ($_SESSION['tempo']== NULL){
+            $_SESSION['tempo']=0;
+        };
+
+    $nivelar = $_SESSION['tempo'];
+    $nivel = 1;
+    $_SESSION['tempo']= $nivel + $nivelar;
+    echo ("sua sessao tem: <br/>  ");
+    echo ($_SESSION['tempo']); 
+    echo (" minutos"); 
+    echo ("l  ");
+    echo("apartir de 40.000.000 você ja esta no nivel épico.");
+?>
 <!DOCTYPE html>
 <html>
 
 	<head>
-		<title>Home - choose</title>
+		<title>Home - escolha</title>
 		<link rel="stylesheet" type="text/css" href="css/style.css"/>
         <style>
-#box1{
-    width:22%; height:30%; 
-    left:10%; top:30%; position:absolute;
-    background-image:url('../../img/boss-1.png');
-    background-size:100%;
-}
-#box2{
-    width:22%; height:30%;
-    left:40%; top:30%; position:absolute; 
-    background-image:url('../../img/boss-2.png');
-    background-size:100%;
-}
-#box3{
-    width:22%; height:30%;
-    left:70%; top:30%; position:absolute; 
-    background-image:url('../../img/boss-3.png');
-    background-size:100%;
-}
-#box4{
-    width:22%; height:30%; 
-    left:10%; top:65%; position:absolute;
-    background-image:url('../../img/boss-4.png');
-    background-size:100%;
-}
-#box5{
-    width:22%; height:30%; 
-    left:40%; top:65%; position:absolute;
-    background-image:url('../../img/boss-5.png');
-    background-size:100%;
-}
-#box6{
-    width:22%; height:30%;
-    left:70%; top:65%; position:absolute; 
-    background-image:url('../../img/boss-6.png');
-    background-size:100%;
-}
-#box1:hover, #box1:active {
-    border: 4px solid white;
-    
-}
-#box2:hover, #box2:active{
-    border: 4px solid white;
-    
-}
-#box3:hover, #box3:active{
-    border: 4px solid white;
-    
-}
-#box4:hover, #box4:active{
-    border: 4px solid white;
-    
-}
-#box5:hover, #box5:active{
-    border: 4px solid white;
-    
-}
-#box6:hover, #box6:active{
-    border: 4px solid white;
-    
-}
+            #box1{
+                width:22%; height:30%; 
+                left:10%; top:30%; position:absolute;
+                background-image:url('../../img/boss-1.png');
+                background-size:100%;
+            }
+            #box2{
+                width:22%; height:30%;
+                left:40%; top:30%; position:absolute; 
+                background-image:url('../../img/boss-2.png');
+                background-size:100%;
+            }
+            #box3{
+                width:22%; height:30%;
+                left:70%; top:30%; position:absolute; 
+                background-image:url('../../img/boss-3.png');
+                background-size:100%;
+            }
+            #box4{
+                width:22%; height:30%; 
+                left:10%; top:65%; position:absolute;
+                background-image:url('../../img/boss-4.png');
+                background-size:100%;
+            }
+            #box5{
+                width:22%; height:30%; 
+                left:40%; top:65%; position:absolute;
+                background-image:url('../../img/boss-5.png');
+                background-size:100%;
+            }
+            #box6{
+                width:22%; height:30%;
+                left:70%; top:65%; position:absolute; 
+                background-image:url('../../img/boss-6.png');
+                background-size:100%;
+            }
+            #box1:hover, #box1:active {
+                border: 4px solid white;
+                
+            }
+            #box2:hover, #box2:active{
+                border: 4px solid white;
+                
+            }
+            #box3:hover, #box3:active{
+                border: 4px solid white;
+                
+            }
+            #box4:hover, #box4:active{
+                border: 4px solid white;
+                
+            }
+            #box5:hover, #box5:active{
+                border: 4px solid white;
+                
+            }
+            #box6:hover, #box6:active{
+                border: 4px solid white;
+                
+            }
         </style>
 	</head>
 
 	<body style="background-image:url('../../img/background.png');background-size:100%; ">
     <div id="princ">
-    <div id="box-TITLE">
+        <div id="box-TITLE">
 
-<a href='../battle/index.php'><img class="solo"  width='40%' height='9.7%'' src='../../img/TITLE.png' style='left:30%; top:10%; margin-right:30% ; position:absolute; border: white 4px solid; '/></a>
-
-</div>
+            <a href="../profile/index.php"><img class="solo"  width='9%' height='9.7%'' src='../../img/profile.png' style='left:15%; top:10%; margin-right:10% ; position:absolute; border: white 4px solid; '/></a>
+                <a href="../home/index.php"/><img class="solo"  width='40%' height='9.7%'' src='../../img/TITLE.png' style='left:30%; top:10%; margin-right:10% ; position:absolute; border: white 4px solid; '/></a>
+            <a href='https://www.facebook.com/media/set/?vanity=abel.barbosa.23&set=a.4155241721221234'><img class="solo"  width='4%' height='9.7%'' src='../../img/doc.png' style='left:80%; top:10%; margin-right:10% ; position:absolute; border: white 4px solid; '/></a>
+        </div>
+            
         <a href='../battle/boss1.php'>
-        <div id="box1">
+            <div id="box1">
 
-             
+                
 
-        </div></a>
+
+            </div>
+        </a>
 
 
         <a href='../battle/boss2.php'>
-        <div id="box2">
+            <div id="box2">
 
-            
+                
 
-        </div></a>
+            </div>
+        </a>
 
-        <a href='../battle/boss3.php'><div id="box3">
-
-
-
-</div></a>
+        <a href='../battle/boss3.php'>
+            <div id="box3">
 
 
 
-<a href='../battle/boss4.php'><div id="box4">
+            </div>
+        </a>
 
 
 
-</div></a>
-<a href='../battle/boss5.php'><div id="box5">
+        <a href='../battle/boss4.php'>
+            <div id="box4">
 
 
 
-</div> </a>
-<a href='../battle/boss6.php'><div id="box6">
+            </div>
+        </a>
+        <a href='../battle/boss5.php'>
+            <div id="box5">
 
 
 
-</div></a>
-    </div>
+            </div> 
+        </a>
+        <a href='../battle/boss6.php'>
+            <div id="box6">
+
+
+
+            </div>
+        </a>
+        </div>
     </body>
 
 </html>
