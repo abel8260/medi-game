4cbe9309fba27eef19ba523907a341e61a9ccfd9
abel8260 <abelvbarbosa@gmail.com>
2021-08-11 07:08:38 -0300

    v(1.00.08)

--- index.php
+++ index.php
@@ -1,18 +1,45 @@
-<!DOCTYPE html>
-<html>
+    <!--html-files----------->
+    <!--definiçao-de-biblioteca-->
+    <!----------------------->
 
-	<head>
-
-		<title>Entrar</title>
-
-		<link rel="stylesheet" type="text/css" href="css/style.css"/>
-
-	</head>
-
-	<body style="background-image:url('../img/background.png');background-size:100%; ">
-
-        <a href='../src/home/index.php'><img class="solo"  width='33%' height='auto'' src='../img/IN.png' style='left:35%; top:30%; position:absolute; '/></a>
+		<!DOCTYPE html>
 		
-	</body>
+		<html> <!-- open the html tag to recognize the html language the  other is from the file scpecilly-->  
 
-</html>
+			<head> <!--this is where stands the title and other things much important of html file -->   
+
+				<title>Home - escolha</title> <!--this is where stand s the title f web fle application-->   
+
+				<!--html-files------------------->
+				<!--Note: in html is im---------->
+				<!--portant that we open--------->
+				<!-- and close the tags-----------
+				even that there is no erason to it.  
+				---------------------------------->
+				<!-----------fim:-html-files------>
+
+				
+			</head>
+			<!----------------------->
+            <!--fim:-head------------>
+                <!--html-files----------->
+                <!--grupo---------------->
+                <!----------------------->
+					<body style="background-image:url('../img/background.png');background-size:100%; ">
+
+						<a href='../src/home/index.php'><img class="solo"  width='33%' height='auto'' src='../img/IN.png' style='left:35%; top:30%; position:absolute; '/></a>!--linkagem----------------->
+						<!----------------------->
+						<!--fim:-a--------------->
+					</body>
+                    <!----------------------->
+                    <!--fim:-body------------->
+                <!----------------------->
+                <!--fim:-grupo----------->
+        </html>
+        <!---------------->
+        <!--DOCTYPE------->
+
+    <!----------------------->
+    <!--fim:-php-files------->
+<!----------------------->
+<!--fim:-php-files------->
